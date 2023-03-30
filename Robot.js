66e4a06b80adb514b6b643e3a6d63dd0d02389e3
Eorg1337 <ya.homutsk@yandex.ru>
2023-03-30 17:56:52 +0300

    new changes

--- Robot.js
+++ Robot.js
@@ -1,10 +1,11 @@
+var currentPosition = {x: 0, y: 0};
+var lastPosition = {};
 function moveRobot() {
     var commands = document.getElementById('commands').value;
-    var robot = document.getElementById('robot');
-    var currentPosition = {x: 0, y: 0};
-    
+    var robot = document.getElementById('robot'); 
     var commandsArray = commands.split(',');
-    
+    console.log(lastPosition)
+    console.log(currentPosition)
     for (var i = 0; i < commandsArray.length; i++) {
     var command = commandsArray[i];
     
@@ -12,10 +13,13 @@ function moveRobot() {
     currentPosition.y++;
     }
     else if (command === 'r') {
-    currentPosition.x++;
+    currentPosition.x++;    
     }
     else if (command === 'l') {
     currentPosition.x--;
+    }
+    else if (command === "def"){
+    currentPosition = {x: 0, y: 0};
     }
     else {
     alert('Ошибка: неверный символ команды');
@@ -23,11 +27,14 @@ function moveRobot() {
     }
     
     if (currentPosition.x < 0 || currentPosition.x > 9 || currentPosition.y < 0 || currentPosition.y > 9) {
-    alert('Ошибка: робот вышел за пределы поля');
+    alert('Ошибка: робот вышел за пределы поля, введите команды правильно');
+    currentPosition = lastPosition;
     return;
     }
-    
     robot.style.top = currentPosition.y * 50 + 'px';
     robot.style.left = currentPosition.x * 50 + 'px';
+    for(let key in currentPosition){
+    lastPosition[key] = currentPosition[key];
     }
-    }
+}
+}
