db08353f6f1ee0ced4d4323ba1193c5c5aa72f3f
Eorg1337 <ya.homutsk@yandex.ru>
2023-04-05 11:10:36 +0300

    new changes

--- Robot.js
+++ Robot.js
@@ -4,61 +4,61 @@ const robot = {
     direction: 'right'
     };
     function moveRobot(command) {
-    switch(command) {
-    case 'f':
-    switch(robot.direction) {
-    case 'right':
-    robot.x += 1;
-    break;
-    case 'left':
-    robot.x -= 1;
-    break;
-    case 'up':
-    robot.y -= 1;
-    break;
-    case 'down':
-    robot.y += 1;
-    break;
+            switch(command) {
+            case 'f':
+        switch(robot.direction) {
+        case 'right':
+        robot.x += 1;
+        break;
+        case 'left':
+        robot.x -= 1;
+        break;
+        case 'up':
+        robot.y -= 1;
+        break;
+        case 'down':
+        robot.y += 1;
+        break;
     }
     break;
-    case 'r':
-    switch(robot.direction) {
-    case 'right':
-    robot.y +=1,
-    robot.direction = 'down';
-    break;
-    case 'left':
-    robot.y -=1;
-    robot.direction = 'up';
-    break;
-    case 'up':
-    robot.x +=1,
-    robot.direction = 'right';
-    break;
-    case 'down':
-    robot.x -=1,
-    robot.direction = 'left';
-    break;
+            case 'r':
+        switch(robot.direction) {
+        case 'right':
+        robot.y +=1,
+        robot.direction = 'down';
+        break;
+        case 'left':
+        robot.y -=1;
+        robot.direction = 'up';
+        break;
+        case 'up':
+        robot.x +=1,
+        robot.direction = 'right';
+        break;
+        case 'down':
+        robot.x -=1,
+        robot.direction = 'left';
+        break;
     }
     break;
-    case 'l':
-    switch(robot.direction) {
-    case 'right':
-    robot.y -=1,
-    robot.direction = 'up';
-    break;
-    case 'left':
-    robot.y +=1;
-    robot.direction = 'down';
-    break;
-    case 'up':
-    robot.x -=1,
-    robot.direction = 'left';
-    break;
-    case 'down':
-    robot.x +=1,
-    robot.direction = 'right';
-    break;
+            case 'l':
+        switch(robot.direction) {
+        case 'right':
+        robot.y -=1,
+        robot.direction = 'up';
+        break;
+        case 'left':
+        robot.y +=1;
+        robot.direction = 'down';
+        break;
+        case 'up':
+        robot.x -=1,
+        robot.direction = 'left';
+        break;
+        case 'down':
+        robot.x +=1,
+        robot.direction = 'right';
+        break;
     }
     break;
     }
@@ -106,6 +106,10 @@ const robot = {
         const commands = input.split("");
         for(let i = 0; i < commands.length; i++) {
         const command = commands[i].trim();
+        if(!command){
+        alert('Введена пустая команда')
+        console.log('Введена пустая команда')
+        }
         if(command !== 'f' && command !== 'r' && command !== 'l'&& command!=',' && command!='.') {
         alert(`Ошибка в лексическом анализе "${command}"`);
         console.log(`Ошибка в лексическом анализе "${command}"`)
